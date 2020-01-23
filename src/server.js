6173f6724fd93457082895a7ea628a4ebe0f6196
Jared Jewitt <jaredjewitt@gmail.com>
2020-01-23 12:06:20 -0800

    Add mongoose and basic gql query

--- src/server.js
+++ src/server.js
@@ -1,1 +1,44 @@
-console.log('Hello World');
+import mongoose from 'mongoose';
+import { ApolloServer, gql } from 'apollo-server';
+
+import { PostModel } from './models';
+
+const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+  type Query {
+    getPosts: [Post]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    async getPosts() {
+      try {
+        return await PostModel.find();
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+  }
+};
+
+const PORT = process.env.PORT || 5000;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+mongoose
+  .connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: PORT });
+  })
+  .then(({ url }) => {
+    console.log(`Server running at ${url}`);
+  });
